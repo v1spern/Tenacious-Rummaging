5fe2d0147e7c58d71d9c32e2e55e9f6582ff0f75
v1sper <joachim86@gmail.com>
2025-10-24 19:53:08 +0200

    version increase

--- config.cpp
+++ config.cpp
@@ -22,7 +22,7 @@ class CfgMods
         name = "Tenacious Rummaging";
         author = "v1sper";
         authorID = "76561197992870768";
-        version = "1.0.4-dev";
+        version = "1.0.4b-dev";
         type = "mod";
 
         dependencies[] = {"Game","World","Mission"};
